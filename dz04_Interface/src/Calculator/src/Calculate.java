dc7f76f69fb2521a4d8580ff9dda94d594025229
fraplO <122119662+fraplO@users.noreply.github.com>
2023-03-13 12:35:43 +0300

    Add files via upload

---
+++ dz04_Interface/src/Calculator/src/Calculate.java
@@ -0,0 +1,23 @@
+public class Calculate
+{
+public static int _sum(int b,int c)
+{
+    return b+c;
+}
+    public static int _minus(int b,int c)
+    {
+        return b-c;
+    }
+    public static int _del(int b,int c)
+    {
+        return b/c;
+    }
+    public static int _multi(int b,int c)
+    {
+        return b*c;
+    }
+    public static double _degree(int b, int c)
+    {
+        return Math.pow(b,c);
+    }
+}
---
+++ dz04_Interface/src/Calculator/src/Proverka.java
@@ -0,0 +1,9 @@
+public class Proverka
+{
+public static void _sumProverka()
+        {
+        }
+
+
+
+}
